f67943638122fba4939fedf66553ab3d79746581
Jpark <web.jpark@gmail.com>
2023-11-15 14:45:54 -0700

    implementing the Cities list

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
@@ -5,8 +6,31 @@ import Product from './pages/Product';
 import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
+import CityList from './components/CityList';
 
 export default function App() {
+	const [cities, setCities] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		const fetchCities = async () => {
+			try {
+				setIsLoading(true);
+				const citiesApiUrl = import.meta.env.VITE_APP_CITIES_API_URL;
+				const citiesEndpoint = import.meta.env.MODE === 'production' ? `${citiesApiUrl}/cities.json` : `${citiesApiUrl}/cities`;
+
+				const data = await (await fetch(citiesEndpoint)).json();
+				setCities(data);
+			} catch (err) {
+				console.log(err.message);
+				alert('There was an error loading data...', err.message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		fetchCities();
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
@@ -15,8 +39,8 @@ export default function App() {
 				<Route path="product" element={<Product />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
-					<Route index element={<p>Default Contents</p>} />
-					<Route path="cities" element={<p>city of the list</p>} />
+					<Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+					<Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
 					<Route path="countries" element={<p>country list</p>} />
 					<Route path="form" element={<p>form</p>} />
 				</Route>
