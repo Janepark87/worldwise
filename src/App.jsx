dbc7111b73ebecd23abc39fb29f1cf149e8fcd93
Jpark <web.jpark@gmail.com>
2023-11-15 18:00:37 -0700

    implementing the UI for the city and form and updating routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
@@ -7,7 +7,9 @@ import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 import CityList from './components/CityList';
+import City from './components/City';
 import CountryList from './components/CountryList';
+import Form from './components/Form';
 
 export default function App() {
 	const [cities, setCities] = useState([]);
@@ -40,10 +42,11 @@ export default function App() {
 				<Route path="product" element={<Product />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
-					<Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+					<Route index element={<Navigate to="cities" replace />} />
 					<Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+					<Route path="cities/:id" element={<City />} />
 					<Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-					<Route path="form" element={<p>form</p>} />
+					<Route path="form" element={<Form />} />
 				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,14 +1,23 @@
+import { Link } from 'react-router-dom';
 import styles from './styles/CityItem.module.css';
 
 export default function CityItem({ city }) {
-	const { cityName, emoji, date } = city;
+	const {
+		id,
+		cityName,
+		emoji,
+		date,
+		position: { lat, lng },
+	} = city;
 
 	return (
-		<li className={styles.cityItem}>
-			<span className={styles.emoji}>{emoji}</span>
-			<h3 className={styles.name}>{cityName}</h3>
-			<time className={styles.date}>({formatDate(date)})</time>
-			<button className={styles.deleteBtn}>&times;</button>
+		<li>
+			<Link to={`${id}?lat=${lat}&lng=${lng}`} className={styles.cityItem}>
+				<span className={styles.emoji}>{emoji}</span>
+				<h3 className={styles.name}>{cityName}</h3>
+				<time className={styles.date}>({formatDate(date)})</time>
+				<button className={styles.deleteBtn}>&times;</button>
+			</Link>
 		</li>
 	);
 }
