07b519b71f1c1204ab7107fe4e929a6cffa37127
Jpark <web.jpark@gmail.com>
2023-12-12 15:15:36 -0700

    Updates

---
+++ assets/PageNotFound-a1f8e947.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-81ce6be4.js";import{M as n,S as r}from"./Section-9a75756b.js";import"./Logo-6d6cfb20.js";function s(){return o.jsx(n,{children:o.jsx(r,{children:o.jsx("h1",{children:"Page not found 😢"})})})}export{s as default};
